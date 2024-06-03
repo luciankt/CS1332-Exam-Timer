93b8fe7e04dbc8e59f8b7e0444e40080bdf97761
Lucian T <30455406+luciankt@users.noreply.github.com>
2024-06-03 18:57:46 -0400

    Panels

---
+++ src/components/LeftPane.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './css/LeftPane.css';
+
+interface LeftPaneProps {
+    // Define the props for the LeftPane component here
+}
+
+const LeftPane: React.FC<LeftPaneProps> = () => {
+    // Implement the component logic here
+
+    return (
+        <div className='leftPane'>
+            {/* Add your JSX content here */}
+        </div>
+    );
+};
+
+export default LeftPane;
---
+++ src/components/RightPane.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './css/RightPane.css';
+
+interface RightPaneProps {
+    // Define the props for the RightPane component here
+}
+
+const RightPane: React.FC<RightPaneProps> = () => {
+    // Implement the component logic here
+
+    return (
+        <div className='rightPane'>
+            {/* Add your JSX content here */}
+        </div>
+    );
+};
+
+export default RightPane;
