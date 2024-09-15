b46f7d09acdae3b6988bc568f64ebd8dc02df6f8
Lucian T <30455406+luciankt@users.noreply.github.com>
2024-09-15 13:45:45 -0400

    fixes maybe

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 import express from 'express';
-import cors from 'cors';
-import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import fs from 'fs';
+import cors from 'cors';
 
-// Convert __dirname to work with ES modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
 const port = 8080;
-const ip = "https://cs1332-exam-timer-407797320918.us-central1.run.app";
+const ip = "https://cs1332-exam-timer-407797320918.us-east1.run.app";
 
-app.use(cors());
+app.use(cors({
+    origin: 'https://cs1332-exam-timer-407797320918.us-east1.run.app'
+}));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'exam-timer/build')));
+app.use(express.static(path.join(__dirname, 'exam-timer/dist')));
 
 // Initialize two string messages
 const configPath = path.join(__dirname, './config.json');
@@ -27,6 +28,7 @@ let instructions = {
 
 // GET endpoint to retrieve the messages
 app.get('/messages', (req, res) => {
+  console.log(instructions)
   res.json(instructions);
 });
 
@@ -35,6 +37,11 @@ app.post('/messages', (req, res) => {
   const { before, during, after } = req.body;
   instructions = { before, during, after };
   res.json({ message: 'Instructions updated successfully' });
+});
+
+// The "catchall" handler: for any request that doesn't match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'exam-timer', 'dist', 'index.html'));
 });
 
 app.listen(port, () => {
